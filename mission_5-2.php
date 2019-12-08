b98193e2a7482de9bc58a0e1a1c6781e1c1fcbe7
akhryht <58671150+akhryht@users.noreply.github.com>
2019-12-09 08:57:43 +0900

    Update mission_5-2.php

--- mission_5-2.php
+++ mission_5-2.php
@@ -33,23 +33,23 @@ if(isset($_POST["delete"]) && $_POST["delete"] !== ""){
     $lines = $stmt->fetchAll();
     foreach($lines as $value){
         if($delete == $value['id']){ 
-        	//データ内のパスワードを取得
-	        $get_pwd = $value['pwd'];
-	        if($get_pwd == $pwd_de){
-	            //削除する
-	            $id = $value['id'];
+            //データ内のパスワードを取得
+	    $get_pwd = $value['pwd'];
+	    if($get_pwd == $pwd_de){
+	        //削除する
+	        $id = $value['id'];
                 $sql = 'delete from akiodb where id=:id';
                 $stmt = $pdo->prepare($sql);
                 $stmt->bindParam(':id', $id, PDO::PARAM_INT);
                 $stmt->execute();
                 echo "削除されました。".'<br/>';
                 $get_pwd = "";
-	        }else{
-	            echo "パスワードが違います。".'<br/>';
-	            $get_pwd = "";
-	        }
+	    }else{
+	        echo "パスワードが違います。".'<br/>';
+	        $get_pwd = "";
 	    }
 	}
+    }
 }
 ?>
 <?php
@@ -65,63 +65,62 @@ if(isset($_POST['choose']) && $_POST['choose'] !== ""){
     $lines = $stmt->fetchAll();
     foreach($lines as $value){
         if($choose == $value['id']){ 
-        	//データ内のパスワード取得
-	        $get_pwd = $value['pwd'];
-	        if($get_pwd == $pwd_up){
-  		  		//その投稿の「名前」と「コメント」, 編集認識用番号を取得
-	            $get_num = $value['id'];
-	            $get_name = $value['name'];
-	            $get_comment = $value['comment'];
-	            $get_date = "";
-	        }else{
-	            echo "パスワードが違います。".'<br/>';
-	            $get_pwd = "";
-	        }
+            //データ内のパスワード取得
+	    $get_pwd = $value['pwd'];
+	    if($get_pwd == $pwd_up){
+  	        //その投稿の「編集認識用番号」, 「名前」, 「コメント」を取得
+	        $get_num = $value['id'];
+	        $get_name = $value['name'];
+	        $get_comment = $value['comment'];
+	        $get_date = "";
+	    }else{
+	        echo "パスワードが違います。".'<br/>';
+	        $get_pwd = "";
 	    }
 	}
+    }
 }
-
-//編集送信処理(入力したデータをupdateで編集)
- if(isset($_POST['update']) && $_POST['update'] !== ""){
- 	echo "編集完了しました。".'<br/>';
- 	//空でないときは、ファイルの中身を取り出し
- 	$update_name = $_POST['name'];
- 	$update_comment = $_POST['comment'];
- 	$update_num = $_POST['update'];
- 	$update_pwd = $_POST['pwd_new'];
+//編集送信処理(入力したデータをupdateで編集)※編集認識用番号の有無で条件分岐
+if(isset($_POST['update']) && $_POST['update'] !== ""){
+    echo "編集完了しました。".'<br/>';
+    //空でないときは、ファイルの中身を取り出し
+    $update_name = $_POST['name'];
+    $update_comment = $_POST['comment'];
+    $update_num = $_POST['update'];
+    $update_pwd = $_POST['pwd_new'];
     $sql = 'SELECT * FROM akiodb';
     $stmt = $pdo->query($sql);
     $lines = $stmt->fetchAll();
     foreach($lines as $value){
         if($update_num == $value['id']){ 
- 			//一致した時のみ、値を入れ替える
- 			$id = $update_num;
- 			$name = $update_name;
- 			$comment = $update_comment;
- 			//$value[3] = $formtime;
- 			$pwd = $update_pwd;
- 			$sql = 'update akiodb set name=:name,comment=:comment,pwd=:pwd_new where id=:id';
-			$stmt = $pdo->prepare($sql);
-			$stmt->bindParam(':name', $name, PDO::PARAM_STR);
-			$stmt->bindParam(':comment', $comment, PDO::PARAM_STR);			
-			$stmt->bindParam(':pwd_new', $pwd, PDO::PARAM_STR);
-			$stmt->bindParam(':id', $id, PDO::PARAM_INT);
-			$stmt->execute();
- 		}
+ 	    //一致した時のみ、値を入れ替える
+ 	    $id = $update_num;
+ 	    $name = $update_name;
+ 	    $comment = $update_comment;
+ 	    //$value[3] = $formtime;
+ 	    $pwd = $update_pwd;
+ 	    $sql = 'update akiodb set name=:name,comment=:comment,pwd=:pwd_new where id=:id';
+	    $stmt = $pdo->prepare($sql);
+	    $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+	    $stmt->bindParam(':comment', $comment, PDO::PARAM_STR);			
+	    $stmt->bindParam(':pwd_new', $pwd, PDO::PARAM_STR);
+	    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+	    $stmt->execute();
  	}
- }else{
+    }
+}else{
 //編集でなければ新規投稿処理(作成したテーブルにinsertを用いてデータを入力)
- 	if(isset($_POST['name'], $_POST['comment']) && $_POST['name'] !=="" && $_POST['comment'] !==""){
-		$sql = $pdo -> prepare("INSERT INTO akiodb (name, comment, pwd) VALUES (:name, :comment, :pwd_new)");
-		$sql -> bindParam(':name', $name, PDO::PARAM_STR);
-		$sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
-		$sql -> bindParam(':pwd_new', $pwd, PDO::PARAM_STR);
-		$name = $_POST['name'];
-		$comment = $_POST['comment'];
-		$pwd = $_POST['pwd_new'];
-		$sql -> execute();
-		echo "新規投稿を受け付けました。".'<br/>';
-	}
+    if(isset($_POST['name'], $_POST['comment']) && $_POST['name'] !=="" && $_POST['comment'] !==""){
+        $sql = $pdo -> prepare("INSERT INTO akiodb (name, comment, pwd) VALUES (:name, :comment, :pwd_new)");
+	$sql -> bindParam(':name', $name, PDO::PARAM_STR);
+	$sql -> bindParam(':comment', $comment, PDO::PARAM_STR);
+	$sql -> bindParam(':pwd_new', $pwd, PDO::PARAM_STR);
+	$name = $_POST['name'];
+	$comment = $_POST['comment'];
+	$pwd = $_POST['pwd_new'];
+	$sql -> execute();
+	echo "新規投稿を受け付けました。".'<br/>';
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -151,10 +150,10 @@ $sql = 'SELECT * FROM akiodb';
 $stmt = $pdo->query($sql);
 $results = $stmt->fetchAll();
 foreach($results as $row){
-	echo $row['id'].',';
-	echo $row['name'].',';
-	echo $row['comment'];
-	echo "<hr>";
+    echo $row['id'].',';
+    echo $row['name'].',';
+    echo $row['comment'];
+    echo "<hr>";
 }
 ?>
 
